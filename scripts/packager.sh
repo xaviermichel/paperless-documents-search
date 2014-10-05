58fbe661da2534b56437393d9f666969e086a573
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2014-10-05 17:43:22 +0200

    Rename artifacts to avoid colisions

--- scripts/packager.sh
+++ scripts/packager.sh
@@ -4,7 +4,7 @@
 
 # path where are placed files before zipping them.
 # /!\ The name will be root in the final zip
-TMP_RELEASE_DIR=./simple-edm
+TMP_RELEASE_DIR=./simple-data-search
 
 # the final zip file, which is to release
 RELEASE_FINAL_FILE=./simple-data-search-{{VERSION}}.zip
@@ -49,18 +49,15 @@ EDM_CONSTANT_VERSION=$(grep 'APPLICATION_VERSION' 'edm-webapp/src/main/resources
 EDM_PARENT_VERSION=$(grep '<version>' 'pom.xml' | head -1 | sed 's/^.*>\(.*\)<.*$/\1/')
 EDM_CONTRACT_VERSION=$(grep 'version' 'edm-contracts/pom.xml' | head -1 | sed 's/^.*>\(.*\)<.*$/\1/')
 EDM_WEBAPP_VERSION=$(grep '<edm.version>' 'edm-webapp/pom.xml' | head -1 | sed 's/^.*>\(.*\)<.*$/\1/')
-EDM_MIGRATION_VERSION=$(grep '<edm.version>' 'edm-mapping-migration/pom.xml' | head -1 | sed 's/^.*>\(.*\)<.*$/\1/')
 
 xlog DEBUG "Parent version        : ${EDM_PARENT_VERSION}"
 xlog DEBUG "Contracts version     : ${EDM_CONTRACT_VERSION}"
 xlog DEBUG "Webapp version        : ${EDM_WEBAPP_VERSION}"
-xlog DEBUG "Migration version     : ${EDM_MIGRATION_VERSION}"
 xlog DEBUG "Constant code version : ${EDM_CONSTANT_VERSION}"
 
 check_maven_code_version ${EDM_CONSTANT_VERSION} ${EDM_PARENT_VERSION} "simple-edm parent"
 check_maven_code_version ${EDM_CONSTANT_VERSION} ${EDM_CONTRACT_VERSION} "simple-edm contracts"
 check_maven_code_version ${EDM_CONSTANT_VERSION} ${EDM_WEBAPP_VERSION} "simple-edm webapp"
-check_maven_code_version ${EDM_CONSTANT_VERSION} ${EDM_MIGRATION_VERSION} "simple-edm migration"
 
 xlog INFO "Versions sounds pretty good !"
 
@@ -105,18 +102,18 @@ xlog INFO "Preparing release directory"
 mkdir -p ${TMP_RELEASE_DIR}
 
 # main jar
-cp ../edm-webapp/target/*.jar ${TMP_RELEASE_DIR}/simple-edm.jar
+cp ../edm-webapp/target/*.jar ${TMP_RELEASE_DIR}/simple-data-search.jar
 
 # webapp resources (exclude dev ressources)
-mkdir -p ${TMP_RELEASE_DIR}/src/main/webapp
-for directory in $(ls ../edm-webapp/src/main/webapp/resources) ; do
-	if [ "${directory}" = "build" -o "${directory}" = "views" -o "${directory}" = "images" ] ; then
-		cp -r ../edm-webapp/src/main/webapp/resources/${directory} ${TMP_RELEASE_DIR}/src/main/webapp/resources/
-	fi
-done
+#mkdir -p ${TMP_RELEASE_DIR}/src/main/webapp
+#for directory in $(ls ../edm-webapp/src/main/webapp/resources) ; do
+#	if [ "${directory}" = "build" -o "${directory}" = "views" -o "${directory}" = "images" ] ; then
+#		cp -r ../edm-webapp/src/main/webapp/resources/${directory} ${TMP_RELEASE_DIR}/src/main/webapp/resources/
+#	fi
+#done
 
 # embedded scripts
-cp simple-edm-windows-starter.cmd ${TMP_RELEASE_DIR}
+cp simple-data-search-windows-starter.cmd ${TMP_RELEASE_DIR}
 
 # create release file
 zip -r ${RELEASE_FINAL_FILE} ${TMP_RELEASE_DIR}
--- scripts/simple-edm-windows-starter.cmd
+++
@@ -1,1 +0,0 @@
-java -jar simple-edm.jar

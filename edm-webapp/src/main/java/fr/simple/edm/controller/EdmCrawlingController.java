7196477d3f343fac3a65a7b048f9c993ad2d33ae
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2015-09-10 00:37:29 +0200

    refactor: remove unused imports

--- edm-webapp/src/main/java/fr/simple/edm/controller/EdmCrawlingController.java
+++ edm-webapp/src/main/java/fr/simple/edm/controller/EdmCrawlingController.java
@@ -5,7 +5,6 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
--- edm-webapp/src/main/java/fr/simple/edm/controller/EdmDocumentController.java
+++ edm-webapp/src/main/java/fr/simple/edm/controller/EdmDocumentController.java
@@ -4,7 +4,6 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;

374d3e2120a8de8231200c2496ddbb8550aff1ec
xaviermichel <xavier.michel.mx440@gmail.com>
2016-05-15 21:00:52 +0200

    feat: prettify code

--- edm-webapp/src/test/resources/static/js/e2e/test-document-preview.js
+++ edm-webapp/src/test/resources/static/js/e2e/test-document-preview.js
@@ -13,7 +13,7 @@ casper.test.begin('Search - document preview should contains all needed informat
         this.test.assertExists(".spec-search-result .spec-search-icon.icone-sprite.word");
         this.test.assertSelectorHasText('.spec-search-result .spec-search-title', 'demo_doc');
         this.test.assertSelectorHasText('.spec-search-result .spec-search-path', '/host_mount_point/demo_2/demo_doc.docx');
-        this.test.assertSelectorHasText('.spec-search-result .spec-search-date', '/' /* '07/04/2016' one day js will have a real date object... */);
+        this.test.assertSelectorHasText('.spec-search-result .spec-search-date', '/' /* '07/04/2016' one day js will have a real date object... */ );
         this.test.assertSelectorHasText('.spec-search-result .spec-search-label', 'demo_category_2');
         this.test.assertSelectorHasText('.spec-search-result .spec-search-content', 'This is a demo doc');
     });

3da9e69723f2d7a0452e863b9ecb60c272e99d64
xmichel <xavier.michel@neo9.fr>
2020-03-08 20:44:58 +0100

    ca: new UI crawler

--- automatic-document-managment/plugins/credit-agricole-csv/dl_invoice.js
+++ automatic-document-managment/plugins/credit-agricole-csv/dl_invoice.js
@@ -24,7 +24,7 @@ log.info(`Getting data for user ${caUser}`);
     const page = await browser.newPage();
     await page.setViewport({
         width: 1920,
-        height: 600
+        height: 800
     });
 
     browser.on('targetcreated', async(target) => {
@@ -38,61 +38,53 @@ log.info(`Getting data for user ${caUser}`);
     });
 
     log.info("Going to CA website");
-    await page.goto('https://www.ca-franchecomte.fr/');
-    await pupeeteerUtils.clickByText(page, 'Accéder à mes comptes', 'span');
-
-    await page.waitForNavigation();
+    await page.goto('https://www.credit-agricole.fr/ca-franchecomte/particulier/acceder-a-mes-comptes.html');
 
     log.info("Filling form");
     await page.waitForSelector("input[name=CCPTE]");
     await page.type('input[name=CCPTE]', caUser, {delay: 25});
+    await pupeeteerUtils.clickByText(page, 'Entrer mon code personnel', 'button');
+
     async function fillPasswordGrid(passwordArray) {
         for (const e of passwordArray) {
             //console.log(`clicking on ${e}`);
-            await pupeeteerUtils.clickByText(page, e, 'a', 0, true);
+            await pupeeteerUtils.clickByText(page, e, 'div', 0, true);
         }
     }
+
+    await page.waitFor(1000);
     await fillPasswordGrid(caPass.split(''));
-    await pupeeteerUtils.clickByText(page, 'Confirmer');
+    await page.click('#validation');
 
     await page.waitForNavigation();
 
     log.info("Going on téléchargement page");
-    await pupeeteerUtils.clickByText(page, 'Téléchargement', 'a');
+    await page.goto('https://www.credit-agricole.fr/ca-franchecomte/particulier/operations/operations-courantes/telechargement.html#!/');
 
-    await page.waitForNavigation();
+    log.info("Choosing accounts");
+    await pupeeteerUtils.clickByText(page, 'Compte de Dépôt', 'strong', 0);
+    await pupeeteerUtils.clickByText(page, 'Compte de Dépôt', 'strong', 1);
 
     log.info("Choosing CSV format");
-    await page.select('#TEL1_TYPE', '3');
+    await page.click('button[data-id=input1]'); // open select
+    await page.waitFor(1000);
+    await pupeeteerUtils.clickByText(page, 'CSV', 'span');
 
     log.info("Choosing custom period");
-    await page.click('.radioBt[value=INTERVAL]');
+    await page.click('date-picker[label=Du]');
 
     log.info('Filling form');
-    await pupeeteerUtils.clearInput(page, 'input[name=TEL1_D_J]');
-    await page.type('input[name=TEL1_D_J]', dateFrom.split('/')[0], {delay: 25});
-    await pupeeteerUtils.clearInput(page, 'input[name=TEL1_D_M]');
-    await page.type('input[name=TEL1_D_M]', dateFrom.split('/')[1], {delay: 25});
-    await pupeeteerUtils.clearInput(page, 'input[name=TEL1_D_A]');
-    await page.type('input[name=TEL1_D_A]', dateFrom.split('/')[2], {delay: 25});
-    await pupeeteerUtils.clearInput(page, 'input[name=TEL1_F_J]');
-    await page.type('input[name=TEL1_F_J]', dateTo.split('/')[0], {delay: 25});
-    await pupeeteerUtils.clearInput(page, 'input[name=TEL1_F_M]');
-    await page.type('input[name=TEL1_F_M]', dateTo.split('/')[1], {delay: 25});
-    await pupeeteerUtils.clearInput(page, 'input[name=TEL1_F_A]');
-    await page.type('input[name=TEL1_F_A]', dateTo.split('/')[2], {delay: 25});
-
-    log.info("Validating form");
-    await pupeeteerUtils.clickByText(page, 'Confirmer', 'a');
-
-    await page.waitForNavigation();
+    await pupeeteerUtils.clearInput(page, '#dateFrom');
+    await page.type('#dateFrom', dateFrom, {delay: 25});
+    await pupeeteerUtils.clearInput(page, '#dateTo');
+    await page.type('#dateTo', dateTo, {delay: 25});
 
     log.info("Downloading file");
-    await pupeeteerUtils.clickByText(page, 'Télécharger', 'a');
+    await pupeeteerUtils.clickByText(page, 'Valider', 'button');
 
     await page.waitFor(10000);
     log.info("Re-downloading file");
-    await pupeeteerUtils.clickByText(page, 'Télécharger', 'a');
+    await pupeeteerUtils.clickByText(page, 'Valider', 'button');
 
     log.info("Wait for download to be finished before closing browser");
     await page.waitFor(20000);
